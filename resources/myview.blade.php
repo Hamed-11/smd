bc23f40d49c8e8c247d535bdc8d626d16774e2d8
Hamed-11 <samadiancfu@gmail.com>
2025-03-02 21:38:43 +0330

    Create myview.blade.php

---
+++ resources/myview.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>صفحه من</title>
+</head>
+<body>
+    <h1>سلام! این یک صفحه تستی است.</h1>
+</body>
+</html>
