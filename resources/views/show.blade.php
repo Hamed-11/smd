32e0a0007bd3ab07c70250b0a45824dcfeec6fc5
Hamed-11 <samadiancfu@gmail.com>
2025-03-08 22:36:28 +0330

    Update show.blade.php

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -12,4 +12,28 @@
     <p><strong>مدت زمان:</strong> {{ $course->duration }} ساعت</p>
     <a href="{{ route('courses.index') }}">بازگشت به لیست دروس</a>
 </body>
+@extends('layouts.app')
+
+@section('content')
+<h2>درس: {{$course->title }}</h2>
+<p>استاد: {{$course->teacher->name }}</p>
+<h3>دانشجویان ثبت شده :</h3>
+<ul>
+    @foreach($course->students as $student)
+    <li>{{ $student->name }}<li>
+        @endforeach
+</ul>
+<h3>افزودن دانشجوی</h3>
+<form action="{{ route('course.enroll',$course->id)}}" method="POST">
+@csrf
+<label>انتخاب دانشجو:</label>
+<select name="user_id" required>
+    @foreach($students as $student)
+<option value="{{student->id}}">{{ $student->name }}</option>
+@endforeach
+</select>
+<button type="submit">ثبت نام</button>
+</form>
+@endsection
+
 </html>
