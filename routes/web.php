f320a91dd3d10e30097762a06d530407aa38b61e
Hamed-11 <samadiancfu@gmail.com>
2025-03-08 22:17:21 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,19 @@ Route::get('/', function () {
 use App\Http\Controllers\MyController;
 
 Route::get('/my-page', [MyController::class, 'showView']);
+
 use App\Http\Controllers\CourseController;
 
 Route::get('/courses', [CourseController::class, 'index'])->name('courses.index');
 Route::get('/courses/{id}', [CourseController::class, 'show'])->name('courses.show');
->
+
+use App\Http\Controllers\CourseController;
+
+Route::get('/courses', [CourseController::class, 'index'])->name('courses.index');
+Route::get('/courses/create', [CourseController::class, 'create'])->name('courses.create');
+Route::post('/courses', [CourseController::class, 'store'])->name('courses.store');
+Route::get('/courses/{id}', [CourseController::class, 'show'])->name('courses.show');
+Route::post('/courses/{id}/enroll', [CourseController::class, 'enroll'])->name('courses.enroll');
+
+
+?>
